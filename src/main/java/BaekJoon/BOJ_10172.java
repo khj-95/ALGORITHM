fe8e86b9ec5b8a16e2d9b1375540a9a629a9acfb
khj <kim37171@cuk.edu>
2022-11-29 03:21:47 +0900

    BOJ_10172
    String

---
+++ src/main/java/BaekJoon/BOJ_10172.java
@@ -0,0 +1,15 @@
+package BaekJoon;
+
+import java.io.*;
+
+public class BOJ_10172 {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write("|\\_/|\n");
+        bw.write("|q p|   /}\n");
+        bw.write("( 0 )\"\"\"\\\n");
+        bw.write("|\"^\"`    |\n");
+        bw.write("||_/=\\\\__|");
+        bw.flush();
+    }
+}
